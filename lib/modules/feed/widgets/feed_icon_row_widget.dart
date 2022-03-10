1abf54c0a8b6568d6350d10e397870ece5e20760
Mahbubur Rahman <rifat0153@gmail.com>
2022-03-10 21:13:00 +0000

    share functionality added

--- lib/modules/feed/widgets/feed_icon_row_widget.dart
+++ lib/modules/feed/widgets/feed_icon_row_widget.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:spotlas/config/constants.dart';
 import 'package:spotlas/config/size_config.dart';
 import 'package:spotlas/models/feed/feed_list.dart';
-import 'package:spotlas/modules/feed/feed_providers.dart';
 import 'package:spotlas/modules/feed/widgets/feed_like_button.dart';
 
 class FeedIconsRowWidget extends ConsumerWidget {
@@ -39,7 +39,9 @@ class FeedIconsRowWidget extends ConsumerWidget {
           ),
           FeedLikeButton(feed: feed),
           GestureDetector(
-            onTap: () {},
+            onTap: () async {
+              await Share.share(feed.description, subject: feed.authorFullName);
+            },
             child: SvgPicture.asset(
               MyAsssets.paperPlaneBorderIcon,
               height: 24,
@@ -51,4 +53,3 @@ class FeedIconsRowWidget extends ConsumerWidget {
     );
   }
 }
-
